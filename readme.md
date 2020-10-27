9f336db43589fe6c47146b0b191e084141ea7779
nfangxu <nfangxu@gmail.com>
2020-10-27 10:11:56 +0800

    docs: update docs

--- readme.md
+++ readme.md
@@ -1,3 +1,36 @@
 ## E签宝 SDK
-### 文件模板API
-- 通过上传方式上传文件
+
+### Install
+
+    ```
+    composer req fangx/esign
+    ```
+
+### Publish Config
+
+    ```
+    php artisan vendor:publish --provider=\\Fangx\\ESign\\ESignServiceProvider
+    ```
+
+### Usage
+
+```php
+<?php
+
+namespace App\Http\Controllers;
+
+use Fangx\ESign\ESign;
+
+class ESignController
+{
+    public function index(ESign $esign)
+    {
+        // $esign->fileTemplate(); # 文件上传 API
+        // $esign->account();
+        // $esign->identityVerified();
+        // $esign->seal();
+        // $esign->sign();
+    }
+}
+
+```
--- src/ESign.php
+++ src/ESign.php
@@ -16,12 +16,18 @@ namespace Fangx\ESign;
 use Fangx\ESign\Contract\AccountApi;
 use Fangx\ESign\Contract\Client;
 use Fangx\ESign\Contract\FileTemplateApi;
+use Fangx\ESign\Contract\IdentityVerifiedApi;
+use Fangx\ESign\Contract\SealApi;
+use Fangx\ESign\Contract\SignApi;
 
 /**
  * Class ESign.
  *
- * @method FileTemplateApi fileTemplateApi()
+ * @method FileTemplateApi fileTemplate()
  * @method AccountApi account()
+ * @method IdentityVerifiedApi identityVerified()
+ * @method SealApi seal()
+ * @method SignApi sign()
  */
 class ESign
 {
@@ -34,9 +40,9 @@ class ESign
 
     public function __call($name, $arguments)
     {
-        if (interface_exists($classname = 'Fangx\\ESign\\Contract\\' . ucfirst($name))) {
+        if (class_exists($classname = '\\Fangx\\ESign\\Contract\\' . ucfirst($name))) {
             // 直接从 Laravel 容器中获取这个对象, 容器自动回实例化, 并实现对应的依赖注入
-            return app($classname);
+            return new $classname($this->client);
         }
 
         throw new \Exception(sprintf('Call to undefined method [%s]', $name));
